9d4eb0ec9741aa47cc8e798654c91b14d9785ec9
Yi Zhang <zorozy@gmail.com>
2017-11-04 15:31:27 -0500

    viper-lll, tests/examples/token/ERC20.v : special expressions and example

---
+++ tests/examples/token/ERC20.v
@@ -0,0 +1,12 @@
+%pgm(
+  %svdecl(balance, %map(%num256, %address), %private)
+  %svdecl(allowances, %map(%map(%num256, %address), %address), %private)
+  %svdecl(num_issued, %num256, %private),
+  %fdecl(%@payable, deposit, ,%void,
+    %assign(%var(_value), %as_num256(%msg.value))
+    %assign(%var(_sender), %msg.sender)
+    %assign(%mapelem(%svar(balance), %var(sender)), %num256_add(%mapelem(%svar(balance), %var(sender)), %var(_value)))
+    %assign(%svar(num_issued), %num256_add(%svar(num_issued), %var(_value)))
+  )
+)
+
