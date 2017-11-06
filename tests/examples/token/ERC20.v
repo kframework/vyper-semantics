5c1ce1a5180ac40f76c100812cb05e48eae13b01
Yi Zhang <zorozy@gmail.com>
2017-11-06 15:01:39 -0600

    viper-lll, ERC20.v: special functions and example

--- tests/examples/token/ERC20.v
+++ tests/examples/token/ERC20.v
@@ -8,5 +8,12 @@
     %assign(%mapelem(%svar(balance), %var(_sender)), %num256_add(%mapelem(%svar(balance), %var(_sender)), %var(_value)))
     %assign(%svar(num_issued), %num256_add(%svar(num_issued), %var(_value)))
   )
+  %fdecl(, withdraw, %param(_value, %num256) , %bool,
+    %assign(%var(_sender), %msg.sender)
+    %assign(%mapelem(%svar(balance), %var(_sender)), %num256_sub(%mapelem(%svar(balance), %var(_sender)), %var(_value)))
+    %assign(%svar(num_issued), %num256_sub(%svar(num_issued), %var(_value)))
+    %send(%var(_sender), %as_wei_value(%as_num128(%var(_value)), wei))
+    %return(true)
+  )
 )
 
