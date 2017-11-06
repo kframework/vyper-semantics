a9ea472861aa1243f5fe9a7b4ecd1e6f602d47e1
Yi Zhang <zorozy@gmail.com>
2017-11-05 18:13:34 -0600

    viper-lll, ERC20.v: rename and fix bugs

--- tests/examples/token/ERC20.v
+++ tests/examples/token/ERC20.v
@@ -5,7 +5,7 @@
   %fdecl(%@payable, deposit, ,%void,
     %assign(%var(_value), %as_num256(%msg.value))
     %assign(%var(_sender), %msg.sender)
-    %assign(%mapelem(%svar(balance), %var(sender)), %num256_add(%mapelem(%svar(balance), %var(sender)), %var(_value)))
+    %assign(%mapelem(%svar(balance), %var(_sender)), %num256_add(%mapelem(%svar(balance), %var(_sender)), %var(_value)))
     %assign(%svar(num_issued), %num256_add(%svar(num_issued), %var(_value)))
   )
 )
