f6aeb4d553d0ddff757c67cc340c15a4c5cff124
Daejun Park <daejunpark@gmail.com>
2017-11-29 14:41:08 -0600

    fix kviper.sh

--- kviper.sh
+++ kviper.sh
@@ -10,6 +10,6 @@ pgm="$1"
 ast="$pgm".ast
 lll="$pgm".lll
 
-python3 parser/viper_parser.py "$pgm" >"$ast"
+python3 "$dir"/parser/viper_parser.py "$pgm" >"$ast"
 krun -d "$dir"/viper-lll "$ast" | sed 's/.*<lll> \(.*\) <\/lll>.*/\1/' >"$lll"
 krun -d "$dir"/lll-evm   "$lll" | sed 's/.*<evm> ListItem ( \(.*\) ) <\/evm>.*/\1/' | sed 's/ ) ListItem ( / /g' | python3 "$dir"/lll-evm/opcodes2bytecodes.py
