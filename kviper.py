4a472b1eeb302f5940196cf8435675d511d4647e
Daejun Park <daejunpark@gmail.com>
2017-12-03 00:55:58 -0600

    type annotation kviper.py

--- kviper.py
+++ kviper.py
@@ -5,24 +5,24 @@ import os
 import subprocess
 import re
 
-from scripts.viper_parser import main as parse
-from scripts.op2byte import encode as op2byte
+from scripts.viper_parser import main as parse # string -> string
+from scripts.op2byte import encode as op2byte # string list -> bytes
 
 path = os.path.dirname(os.path.realpath(__file__))
 
-def krun(kdir, pgm):
+def krun(kdir, pgm): # string * string -> string
     p = subprocess.run(['krun', '-d', os.path.join(path, kdir), '-cPGM=' + pgm, '-pPGM=kast -e'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
     if p.returncode == 0:
         return p.stdout
     else:
         raise RuntimeError(p.stderr)
 
-def viper2lll(ast):
+def viper2lll(ast): # string -> string
     out = krun('viper-lll', ast)
     lll = re.search(r'<lll> (.*) </lll>', out).group(1)
     return lll
 
-def lll2evm(lll):
+def lll2evm(lll): # string -> string list
     out = krun('lll-evm', lll)
     evm = re.compile(r' \) ListItem \( ').sub(' ', re.search(r'<evm> ListItem \( (.*) \) </evm>', out).group(1))
     return evm.split(' ')
