c9fb7dc0b3f3c643d5a334c6004518a4d8cc3ec7
Yi Zhang <zorozy@gmail.com>
2017-12-05 23:38:36 -0600

    viper-lll: support struct literal

---
+++ tests/viper-lll/parser/syntax/test_struct/valid5.v.py
@@ -0,0 +1,5 @@
+mom: {a: {c: num}[3], b: num}
+nom: {c: num}[3]
+@public
+def foo():
+    self.mom = {a: self.nom, b: 5}
---
+++ tests/viper-lll/parser/syntax/test_struct/valid7.v.py
@@ -0,0 +1,5 @@
+mom: {a: {c: num}[3], b: num}
+@public
+def foo():
+    nom: {c: num}[3]
+    self.mom = {a: nom, b: 5}
