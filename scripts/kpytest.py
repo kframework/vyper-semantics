b0b41057cf1b06b1fb422406d8b689be82ea8c7d
Denis Bogdanas <denis.bogdanas@gmail.com>
2018-03-07 21:29:48 +0200

    viper tools: Disabled gas estimation.

--- scripts/kpytest.py
+++ scripts/kpytest.py
@@ -8,8 +8,21 @@ import os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from kviper import compile
 
+
+def gas_estimate(origcode, *args, **kwargs):
+    o = compiler.gas_estimate(origcode, args, kwargs)
+    o2 = {}
+    for key in o:
+        o2[key] = 1000000000
+    return o2
+
+
 if __name__ == '__main__':
     # patching viper.compiler.Compiler.compile
     compiler.Compiler.compile = lambda self, code, *args, **kwargs: compile(code)
     compiler.compile = lambda code, *args, **kwargs: compile(code)
+
+    # disabling gas estimation
+    compiler.Compiler.gas_estimate = lambda self, code, *args, **kwargs: gas_estimate(code, args, kwargs)
+
     py.test.cmdline.main()  # invoking pytest with args received from command line
