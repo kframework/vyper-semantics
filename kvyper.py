ff4e549b051c811637bbbfb0d5bc9bebb454c237
Denis Bogdanas <denis.bogdanas@gmail.com>
2018-03-18 13:41:02 +0200

    viper-lll: #canonicalizeType() - fixed a regression.

--- kvyper.py
+++ kvyper.py
@@ -12,7 +12,7 @@ path = os.path.dirname(os.path.realpath(__file__))
 
 def krun(kdir, pgm): # string * string -> string
     try:
-        p = subprocess.run(['krun', '-d', os.path.join(path, kdir), '-cPGM=' + pgm, '-pPGM=kast -e'],
+        p = subprocess.run(['krun', '-d', os.path.join(path, kdir), '-cPGM=' + pgm, '-pPGM=kast -e', '--debug'],
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
     except FileNotFoundError as e:
         print("Error: subprocess.run() ended with FileNotFoundError for file: " + str(e.filename), file=sys.stderr)
@@ -25,19 +25,31 @@ def krun(kdir, pgm): # string * string -> string
 
 def vyper2lll(ast): # string -> string
     out = krun('vyper-lll', ast)
-    lll = re.search(r'<lll> (.*) </lll>', out).group(1)
-    if lll == ".":
+    print("\n{}\n\n".format(out))
+
+    if "<k> . </k>" in out:
+        lll = re.search(r'<lll> (.*) </lll>', out).group(1)
+        return lll
+    else:
         raise RuntimeError("vyper-lll computation got stuck:\n\n" + out + "\n\n")
 
-    return lll
 
 def lll2evm(lll): # string -> string list
     out = krun('lll-evm', lll)
-    evm = re.compile(r' \) ListItem \( ').sub(' ', re.search(r'<evm> ListItem \( (.*) \) </evm>', out).group(1))
-    return evm.split(' ')
+    #print("\n{}\n\n".format(out))
+
+    if "<k> . </k>" in out:
+        evm = re.compile(r' \) ListItem \( ').sub(' ', re.search(r'<evm> ListItem \( (.*) \) </evm>', out).group(1))
+        return evm.split(' ')
+    else:
+        raise RuntimeError("lll-evm computation got stuck:\n\n" + out + "\n\n")
 
 def compile(code): # string -> bytes
+    print("\n{}\n\n".format(code))
+
     ast = parse(code)
+    print("\n{}\n\n".format(ast))
+
     lll = vyper2lll(ast)
     evm = lll2evm(lll) # list of opcodes
     return op2byte(evm)
