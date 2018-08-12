04550c6886982b888dae31c1928e054c74caebab
Denis Bogdanas <denis.bogdanas@gmail.com>
2018-08-12 16:36:21 +0300

    Scripts: updated for vyper v0.1.0-beta.2

--- scripts/kpytest.py
+++ scripts/kpytest.py
@@ -3,16 +3,17 @@
 import py
 from vyper import compiler
 
-import sys
 import os.path
 
 # importing parent dir, where kvyper.py is located
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from kvyper_test import compile
 
+old_gas_estimate = compiler.gas_estimate
+
 
 def gas_estimate(origcode, *args, **kwargs):
-    o = compiler.gas_estimate(origcode, args, kwargs)
+    o = old_gas_estimate(origcode, args, kwargs)
     o2 = {}
     for key in o:
         o2[key] = 1000000000
@@ -21,11 +22,10 @@ def gas_estimate(origcode, *args, **kwargs):
 
 if __name__ == '__main__':
     # patching vyper.compiler.Compiler.compile
-    compiler.Compiler.compile = lambda self, code, *args, **kwargs: compile(code)
     compiler.compile = lambda code, *args, **kwargs: compile(code)
 
     # disabling gas estimation
-    compiler.Compiler.gas_estimate = lambda self, code, *args, **kwargs: gas_estimate(code, args, kwargs)
+    compiler.gas_estimate = lambda code, *args, **kwargs: gas_estimate(code, args, kwargs)
 
     # changing working directory to vyper repo. Required for tests that load other files.
     os.chdir("../vyper")
